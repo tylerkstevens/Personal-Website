9baf4ec79c0b80a2ac356ac0483db8100096d43b
Tyler Stevens <tyler.stevens97@gmail.com>
2024-04-15 08:05:20 -0600

    file clean up and organization.

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://astrofy-template.netlify.app',
+  site: 'https://www.tylerstevens.me',
   integrations: [mdx(), sitemap(), tailwind()]
 });
